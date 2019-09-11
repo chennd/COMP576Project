80411009fc4a608026edc3b7c0010320a7c1bcdc
Neil Chen <ndc1@rice.edu>
2019-09-11 05:06:17 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+# This project is a new project in Pycharm meant to be committed/pushed to GitHub as a public project.
+print("Hello, world!")
+
+# Interviewer: What is your biggest strength?
+#
+# Me: I am an expert in machine learning.
+#
+# Interviewer: What’s 6 + 10?
+#
+# Me: Zero.
+#
+# Interviewer: Nowhere near. It’s 16.
+#
+# Me: Ok, It’s 16.
+#
+# Interviewer: What is 10 + 20?
+#
+# Me: It’s 16.
